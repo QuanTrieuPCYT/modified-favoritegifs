5ebf470a276d5026a3bf1231510ada4bd17e71db
castdrian <adrifcastr@gmail.com>
2023-08-04 22:36:53 +0200

    also check attachments

--- plugins/favorite-gifs/src/util.ts
+++ plugins/favorite-gifs/src/util.ts
@@ -1,4 +1,4 @@
-export interface ImageEmbed {
+interface ImageEmbed {
 	id: string;
 	url: string;
 	type: string;
@@ -11,7 +11,7 @@ export interface ImageEmbed {
 	fields: any[];
 }
   
-export interface GifvEmbed {
+interface GifvEmbed {
 	id: string;
 	url: string;
 	type: string;
@@ -35,19 +35,38 @@ export interface GifvEmbed {
 	fields: any[];
 }
   
+interface Attachment {
+	width: number;
+	url: string;
+	size: number;
+	proxy_url: string;
+	id: string;
+	height: number;
+	filename: string;
+	content_type: string;
+	spoiler: boolean;
+}
+  
 export interface Message {
 	embeds: (ImageEmbed | GifvEmbed)[];
+	attachments: Attachment[];
 }
-
+  
 export function getGifUrl(message: Message): string | null {
 	for (let embed of message.embeds) {
 	  if (embed.type === 'gifv') {
 		return (embed as GifvEmbed).url;
-	  }
-	  else if (embed.type === 'image' && embed.url.endsWith('.gif')) {
+	  } else if (embed.type === 'image' && embed.url.endsWith('.gif')) {
 		return (embed as ImageEmbed).url;
+	  }
+	}
+  
+	for (let attachment of message.attachments) {
+	  if (attachment.url.endsWith('.gif')) {
+		return attachment.url;
 	  }
 	}
   
 	return null;
 }
+  
